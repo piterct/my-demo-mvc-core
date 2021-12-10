6e0c3b8e5204bb72a4eb50cb700373c92dd5be2b
Michael Peter <michael_piterct@hotmail.com>
2021-12-10 01:11:41 -0300

    rotes

--- MyDemoMvc/MyDemoMvc/Controllers/HomeController.cs
+++ MyDemoMvc/MyDemoMvc/Controllers/HomeController.cs
@@ -18,7 +18,7 @@ namespace MyDemoMvc.Controllers
             _logger = logger;
         }
 
-        public IActionResult Index()
+        public IActionResult Index(string id, string categorias)
         {
             return View();
         }
