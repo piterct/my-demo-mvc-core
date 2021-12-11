20c1ffc698558dfe00393e78389516fe6a46fee9
Michael Peter <michael_piterct@hotmail.com>
2021-12-11 18:11:29 -0300

    using scaffold

---
+++ MyDemoMvc/MyDemoMvc/Data/MyDemoMvcContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MyDemoMvc.Models;
+
+namespace MyDemoMvc.Data
+{
+    public class MyDemoMvcContext : DbContext
+    {
+        public MyDemoMvcContext (DbContextOptions<MyDemoMvcContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<MyDemoMvc.Models.Livro> Livro { get; set; }
+    }
+}
