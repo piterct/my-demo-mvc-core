dd224ad99ad6c3307254cd9f774a3e3d4ee9e713
Michael Peter <michael_piterct@hotmail.com>
2021-12-11 10:08:04 -0300

    using routes

--- MyDemoMvc/MyDemoMvc/Controllers/HomeController.cs
+++ MyDemoMvc/MyDemoMvc/Controllers/HomeController.cs
@@ -9,26 +9,34 @@ using System.Threading.Tasks;
 
 namespace MyDemoMvc.Controllers
 {
+    [Route("")]
+    [Route("gestao-clientes")]
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
-
+      
         public HomeController(ILogger<HomeController> logger)
         {
             _logger = logger;
         }
 
-        public IActionResult Index(string id, string categorias)
+        [Route("")]
+        [Route("pagina-inicial")]
+        [Route("pagina-inicial/{id:int}/{categoria:guid}")]
+        public IActionResult Index(string id, Guid categoria)
         {
             return View();
         }
 
+        [Route("privacidade")]
+        [Route("politica-de-privacidade")]
         public IActionResult Privacy()
         {
             return View();
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        [Route("erro-encontrado")]
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
